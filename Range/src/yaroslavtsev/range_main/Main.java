4bd924acb99253a4456bab6ec8412f4dc7f94447
Evgeny <yaroslavtseveugene@gmail.com>
2025-01-23 19:59:02 +0700

    Added codes for classes Range and Main

--- Range/src/yaroslavtsev/range_main/Main.java
+++ Range/src/yaroslavtsev/range_main/Main.java
@@ -1,7 +1,31 @@
 package yaroslavtsev.range_main;
 
+import yaroslavtsev.range.Range;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        Random random = new Random();
+        Range mysteryRange = new Range(random.nextDouble(10), random.nextDouble(11) + 10);
+
+        int attemptsCount = 1;
+        System.out.printf("Длина диапазона равна: %f%n", mysteryRange.getLength());
+
+        while (true) {
+            System.out.print("Введите число от 0.0 до 20.0: ");
+            double enteredNumber = scanner.nextDouble();
+
+            if (mysteryRange.isInside(enteredNumber)) {
+                break;
+            }
+
+            System.out.println("Число не в диапазоне");
+            ++attemptsCount;
+        }
+
+        System.out.printf("Введённое значение попало в диапазон. Попыток: %d", attemptsCount);
     }
 }
